8afd06f16f7c5ca893022be77026e3f31588dc19
justine <justine.winnie@gmail.com>
2018-09-22 13:15:57 -0700

    Add logo and basic landing page

--- survey-app/routes/index.js
+++ survey-app/routes/index.js
@@ -35,11 +35,8 @@ var routes = {
 exports = module.exports = function (app) {
 	// Views
 	app.get('/', routes.views.index);
-<<<<<<< HEAD
 	app.get('/survey', routes.views.survey);
-=======
 
->>>>>>> add generated keystone app
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
 
