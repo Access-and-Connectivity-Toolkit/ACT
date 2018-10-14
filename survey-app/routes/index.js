d6b6cde82eeba000c51367b703e997a620b662f5
S. Gallagher <sjgllghr@gmail.com>
2018-10-14 14:17:58 -0700

    fix application start error

--- survey-app/routes/index.js
+++ survey-app/routes/index.js
@@ -47,8 +47,6 @@ exports = module.exports = function (app) {
 
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
 	// app.get('/protected', middleware.requireUser, routes.views.protected);
-	
-	app.all('/signin', routes.views.signin);
 
 	// api routes
 	app.get('/api/resource', apiHandlers.getResources);
