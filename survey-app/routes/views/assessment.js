8e4683f813a228e8d855adae71f2cbd9797597db
S. Gallagher <sjgllghr@gmail.com>
2018-11-04 16:28:16 -0800

    put survey modules on seperate pages

--- survey-app/routes/views/assessment.js
+++ survey-app/routes/views/assessment.js
@@ -3,10 +3,20 @@ exports = module.exports = function (req, res) {
     var view = new keystone.View(req, res);
     var locals = res.locals;
     locals.section = 'assessment';
+    
+    var ModulePath = keystone.list('ModulePath').model;
+    view.query('path', ModulePath.find({'_id': req.user.assignedPath}));
 
     // Find all modules in this user's assigned path
     var Module = keystone.list('Module').model;
     view.query('modules', Module.find({'pathways': req.user.assignedPath}).sort({'index': 'asc'}));
+
+    view.on('get', function(next) {
+        locals.surveyIndex = req.query.surveyIndex;
+        console.log(req.query);
+
+        next();
+    });
 
     view.render('assessment');
 };
