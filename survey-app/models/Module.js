24ded0c69aaa229919e3c7d1a5b4a3f527d1868c
S. Gallagher <sjgllghr@gmail.com>
2018-11-04 17:39:51 -0800

    add resouce area

--- survey-app/models/Module.js
+++ survey-app/models/Module.js
@@ -11,7 +11,8 @@ Module.add({
     name: { type: Types.Text, initial: true, required: true },
     url: { type: Types.Url, initial: true, required: true },
     pathways: { type: Types.Relationship, ref: 'ModulePath', initial: true, required: true, many: true },
-    index: {type: Types.Number, initial: true, required: true, unique: true, index: true}
+    index: {type: Types.Number, initial: true, required: true, unique: true, index: true},
+    resources: { type: Types.Url }
 });
 
 Module.register();
