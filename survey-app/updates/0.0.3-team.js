09da802e5a1984e08bd4f5aa05f50019bd9a32e4
S. Gallagher <sjgllghr@gmail.com>
2019-01-12 06:29:42 -0800

    add initial team

---
+++ survey-app/updates/0.0.3-team.js
@@ -0,0 +1,31 @@
+const keystone = require('keystone');
+const Team = keystone.list('Team');
+const User = keystone.list('User').model;
+
+// Add initial team so user team functionality works
+
+const team = {
+    'name': 'Jefferson County',
+    'state': 'Washington',
+    'leader': 'user@keystonejs.com'
+}
+
+createTeam = async (team) => {
+    const leader = await User.findOne({'email': team.leader});
+    if (!leader) {
+        console.log('Invalid team leader');
+    } else {
+        team.leader = leader._id.toString();
+        const newTeam = new Team.model(team);
+        await newTeam.save();
+        leader.team = newTeam;
+        User.update({_id: leader._id}, {$set: {"team": newTeam._id}}, (err, data) => {
+            if (err) console.log(err);
+        });
+    }
+}
+
+exports = module.exports = async (done) => {
+    await createTeam(team);
+    done();
+}
