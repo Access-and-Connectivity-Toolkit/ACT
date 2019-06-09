99f65ad63e0ee9e6e5ef29716975b2f7bc52e8cb
S. Gallagher <sjgllghr@gmail.com>
2019-06-09 15:14:09 -0700

    change roles

---
+++ survey-app/updates/0.0.9-roles.js
@@ -0,0 +1,15 @@
+exports.create = {
+	Role: [
+        {'name': 'Economic Development Manager'},
+        {'name': 'City Manager'},
+        {'name': 'Mayor/County Commissioner'},
+        {'name': 'CIO/IT Manager'},
+        {'name': 'Librarian'},
+        {'name': 'Educators'},
+        {'name': 'Business Leaders'},
+        {'name': 'Farmers/Ranchers'},
+        {'name': 'Civic Leader'},
+        {'name': 'Local Advocates'},
+        {'name': 'Community Member'},
+	]
+};
