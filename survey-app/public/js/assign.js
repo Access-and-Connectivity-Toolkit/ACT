719fea67fa811dfea980985c47272fce14c15146
S. Gallagher <sjgllghr@gmail.com>
2019-06-30 15:31:09 -0700

    add error handling

--- survey-app/public/js/assign.js
+++ survey-app/public/js/assign.js
@@ -13,19 +13,23 @@ var userFormAssigner = userFormAssigner || (function() {
 		setRolesMap: (map) => {
         	rolesMap = map;
 		},
-		populateCheckboxes: () => {
-        	//TODO: all the checkbox checking shit can go here, for on change + modal on load
-		},
         start: () => {
             $(document).ready(() => {
+                // Hide error message from previous error (if any)
+                $('#modal').on('hide.bs.modal', function(event){ 
+                    $('#modal-error').addClass('hidden');
+                });
+
                 $('#modal').on('show.bs.modal', function(event) {
-                	
+                	// Select appropriate modules based on chosen role
                     $('select#role').change(() => {
-                        //TODO: review - how do we want precedence to work for modules via role assignment vs. direct module assignment
                         const roleId = $('select#role').val();
-						console.log('inside of select role change')
-						console.log('curr role', roleId, 'assigned modules', rolesMap[roleId]);
-						//TODO: what if role ID is invalid? 
+
+                        if (roleId === 'no-role') return;
+                        if (roleId === undefined || rolesMap[roleId] === undefined) {
+                            $('#modal-error').removeClass('hidden');
+                            return;
+                        }
 						let roleModules = rolesMap[roleId].modules;
 						$(':checkbox').each(function() {
 							const moduleId = this.value;
@@ -36,18 +40,9 @@ var userFormAssigner = userFormAssigner || (function() {
 							} else {
 								this.checked = false;
 							}
-							
-							//add additional modules if needed 
-							//TODO: can team leaders choose to un-assign modules that were preassigned by role? 
-							if (userModules[moduleId]) {
-								this.checked = true;
-							} else {
-								this.checked = false;
-							}
 						});
 					});
                     
-                    console.log('hi');
                     const roleId = $(event.relatedTarget).data('role');
                     
                     //selects no role in the dropdown if user doesn't have role yet
@@ -59,19 +54,10 @@ var userFormAssigner = userFormAssigner || (function() {
                     const userId = $(event.relatedTarget).data('user');
                     
                     const userModules = assignedModulesByUser[userId];
-					const roleModules = rolesMap[roleId].modules;
 
                     // Get all modules and check the ones this user has been assigned
                     $(':checkbox').each(function() {
                         let moduleId = this.value;
-
-						//assigns default role modules to user
-						if (roleModules.includes(moduleId)) {
-							this.checked = true;
-						} else {
-							this.checked = false;
-						}
-                        
 						
                         if (userModules[moduleId]) {
                             this.checked = true;
